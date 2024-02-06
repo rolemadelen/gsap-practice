061da1be2951dbadf2ce12ee555a60550f357fc3
rolemadelen <rolemadelen17@pm.me>
2024-02-06 15:30:37 -0500

    pin background images

---
+++ src/page3/part3.js
@@ -0,0 +1,29 @@
+let panels = gsap.utils.toArray('.container');
+let tops = panels.map((panel) =>
+  ScrollTrigger.create({ trigger: panel, start: 'top top' })
+);
+
+panels.forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: () =>
+      panel.offsetHeight < window.innerHeight ? 'top top' : 'bottom bottom',
+    pin: true,
+    pinSpacing: false,
+  });
+});
+
+ScrollTrigger.create({
+  snap: {
+    snapTo: (progress, self) => {
+      let panelStarts = tops.map((st) => st.start),
+        snapScroll = gsap.utils.snap(panelStarts, self.scroll());
+      return gsap.utils.normalize(
+        0,
+        ScrollTrigger.maxScroll(window),
+        snapScroll
+      );
+    },
+    duration: 0.5,
+  },
+});
